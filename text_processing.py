58c0495dd435ecb5b9845fdf5d21f35cd6ecc77c
dldydrhkd <48857296+dldydrhkd@users.noreply.github.com>
2021-01-19 17:11:25 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -61,8 +61,14 @@ def no_vowels(input_string):
     """
     no_vowel_string = ''
     for i in range(len(input_string)):
-        if(input_string[i]=='a' or input_string[i]=='e' or input_string[i]=='i' or input_string[i]=='o' or input_string[i]=='u'):
+        if(input_string[i]=='a' or input_string[i]=='e' or input_string[i]=='i' or input_string[i]=='o' or input_string[i]=='u'
+        or intput_string[i]=='A' or input_string[i]=='E' or input_string[i]=='I' or input_string[i]=='O' or input_string[i]=='U'):
             continue
         else:
             no_vowel_string = no_vowel_string + input_string[i]
     return no_vowel_string
+
+
+
+s = ""
+print(no_vowels(s))
